dec98ebed55a44d6e8b34c281b89751fcd47139f
Luan-art <luanlf2003@gmail.com>
2024-04-19 21:37:16 -0300

    Add project files.

---
+++ hipotenusa/Program.cs
@@ -0,0 +1,26 @@
+﻿double cateto1 = 0, cateto2 = 0, hipotenusa = 0;
+
+do
+{
+    Console.WriteLine("Digite o primeiro cateto: ");
+    cateto1 = float.Parse(Console.ReadLine());
+} while (cateto1 <= 0);
+
+do
+{
+    Console.WriteLine("Digite o segundo cateto: ");
+    cateto2 = float.Parse(Console.ReadLine());
+} while (cateto2 <= 0);
+
+
+if((cateto1 * cateto1) + (cateto2 * cateto2) <= 0)
+{
+    Console.WriteLine("Valores invalidos nos catetos");
+}
+else
+{
+    hipotenusa = Math.Sqrt((cateto1 * cateto1) + (cateto2 * cateto2));
+    Console.WriteLine($"A hipotenusa é {hipotenusa}");
+
+
+}
